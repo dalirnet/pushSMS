ec8f6ded2548196fa6760c93daf01e24d976b3e2
Amir Reza Dalir <dalirnet@gmail.com>
2019-07-23 09:59:44 +0430

    menu update

--- app/src/main/java/com/dalirnet/pushsms/MainActivity.java
+++ app/src/main/java/com/dalirnet/pushsms/MainActivity.java
@@ -1,5 +1,7 @@
 package com.dalirnet.pushsms;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -41,14 +43,12 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.action_git) {
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/dalirnet"));
+            startActivity(browserIntent);
         }
 
         if (id == R.id.action_author) {
